ffe45ab8904a50bf12853d70f852f55587055b0e
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-08-31 19:51:34 +0900

    refactor: prismaクライアントの初期化

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,16 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/lib/prisma/index";
 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-
-const globalForPrisma = globalThis as typeof globalThis & {
-  prisma?: PrismaClient | undefined;
-};
-
-
-export const prisma =
-  globalForPrisma.prisma ??
-  (globalForPrisma.prisma = new PrismaClient());
-
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
