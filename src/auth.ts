bd976fbb0be1ce364fd58cdd885bc8e88ce71084
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-08-31 22:07:55 +0900

    refactor: providerの指定方法変更

--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,11 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/app/lib/prisma";
-import GitHubProvider from "next-auth/providers/github";
+import GitHub from "next-auth/providers/github"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   debug: false,
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
+  providers: [GitHub],
   adapter: PrismaAdapter(prisma),
   callbacks: {
     session: ({ session, user }) => {
