a20c36ff80a56f1d6bdcc1517d9bf0966e8cbdd3
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-09-01 15:52:51 +0900

    feat : microCMSから記事取得

--- src/app/lib/microcms/client.ts
+++ src/app/lib/microcms/client.ts
@@ -1,6 +1,14 @@
+import { BookType } from '@/types';
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN!, 
   apiKey: process.env.NEXT_PUBLIC_API_KYE!,
 });
+
+export const getAllBooks = async () => {
+  const allBooks = await client.getList<BookType>({
+    endpoint: "bookcommerce",
+  });
+  return allBooks;
+};
