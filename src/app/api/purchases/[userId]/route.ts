5cfe423613e01293fb5ddb39a904dfba8abd6bc5
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-09-02 18:11:31 +0900

    feat:購入履歴取得APIの作成

--- src/app/api/purchases/[userId]/route.ts
+++ src/app/api/purchases/[userId]/route.ts
@@ -5,7 +5,7 @@ type Params = {
   params: {userId: string;};
 }
 //購入履歴検索API
-export async function GET (request : Request, { params}: Params) {
+export async function GET (request : Request, {params}: Params) {
   const userId = params.userId;
   try {
     const purchase = await prisma.purchase.findMany({
