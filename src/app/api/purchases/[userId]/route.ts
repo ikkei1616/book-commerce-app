74c6e7d15402a7663b3ef8f7839e9e1f32068f72
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-09-02 17:45:13 +0900

    feat: 購入履歴検索APIの実装

---
+++ src/app/api/purchases/[userId]/route.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
+
+type Params = {
+  params: {userId: string;};
+}
+//購入履歴検索API
+export async function GET (request : Request, { params}: Params) {
+  const userId = params.userId;
+  try {
+    const purchase = await prisma.purchase.findMany({
+      where:{ userId},
+    });
+    return NextResponse.json(purchase);
+  } catch (err) {
+    return NextResponse.json(err);
+  }
+
+};
